cb5d6b7519a66f892810b9c68d6003a979047913
asyakasimova <kilyanet@gmail.com>
2016-09-20 23:11:52 +0300

    1

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,6 +1,9 @@
+/**
+ * Created by Valkyrja on 20.09.2016.
+ */
 public class MyFirstProgram {
-
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
 }
+
