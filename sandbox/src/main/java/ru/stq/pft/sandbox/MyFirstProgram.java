1e34c185ea7e7491afbbbf4c71eaa57b761d0529
asyakasimova <kilyanet@gmail.com>
2016-09-21 23:07:26 +0300

    ДЗ 2. Рассчет расстояния

--- sandbox/src/main/java/ru/stq/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stq/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,36 @@ package ru.stq.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		hello("World");
+
+		Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+ 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+    Point p1 = new Point(-10, 1);
+    Point p2 = new Point(4, 10);
+    System.out.println("Расстояние от точки с координатами " + p1.x + " и " + p1.y + " до точки с координатами " + p2.x + " и " + p2.y + " равно " + p1.distance(p2));
+
+
+		System.out.println(2 + 2);
+		System.out.println(2 * 2);
+		System.out.println(2 - 2);
+		System.out.println(2 / 2);
+
+		System.out.println(1.0 / 2);
+
+		System.out.println("2" + "2");
+		System.out.println("2" + 2);
+
+		System.out.println("2 + 2 = " + (2 + 2));
+
 	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+
 }
