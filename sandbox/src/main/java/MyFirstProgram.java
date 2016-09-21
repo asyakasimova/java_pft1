8eda7409437356724488f200a68ae35d41a597e2
asyakasimova <kilyanet@gmail.com>
2016-09-21 07:10:01 +0300

    Создан пакет для моей первой программы

--- sandbox/src/main/java/MyFirstProgram.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Valkyrja on 20.09.2016.
- */
-public class MyFirstProgram {
-        public static void main(String[] args) {
-            System.out.println("Hello, World!");
-        }
-}
-
