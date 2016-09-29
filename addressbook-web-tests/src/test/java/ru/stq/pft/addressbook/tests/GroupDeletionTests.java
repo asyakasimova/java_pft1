616ebb49ba6bd0fff87a81d6ae63d4431c3984de
asyakasimova <kilyanet@gmail.com>
2016-09-29 14:42:31 +0300

    Реализована проверка и выполнения предусловия для групп

--- addressbook-web-tests/src/test/java/ru/stq/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stq/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.stq.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stq.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase{
 
@@ -8,6 +9,9 @@ public class GroupDeletionTests extends TestBase{
   public void testGroupDeletion() {
 
     app.getNavigationHelper().gotoGroupsPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
