ed2829cba4c224f65bb0513f42d97247f6e361d9
asyakasimova <kilyanet@gmail.com>
2016-10-06 18:16:24 +0300

    Реализована проверка элементов в тесте на создание групп

--- addressbook-web-tests/src/test/java/ru/stq/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stq/pft/addressbook/tests/GroupCreationTests.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 import ru.stq.pft.addressbook.model.GroupData;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase{
@@ -14,8 +16,21 @@ public class GroupCreationTests extends TestBase{
 
     app.getNavigationHelper().gotoGroupsPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test3", null, null);
+    app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    int max = 0;
+    for (GroupData g : after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
