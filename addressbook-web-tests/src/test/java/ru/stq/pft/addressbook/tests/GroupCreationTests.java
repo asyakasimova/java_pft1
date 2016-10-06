c759a098934b9e17bca820e98c994f4c8f1a0d17
asyakasimova <kilyanet@gmail.com>
2016-10-06 18:53:40 +0300

    Реализованы все тесты групп

--- addressbook-web-tests/src/test/java/ru/stq/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stq/pft/addressbook/tests/GroupCreationTests.java
@@ -22,15 +22,9 @@ public class GroupCreationTests extends TestBase{
     Assert.assertEquals(after.size(), before.size() + 1);
 
     before.add(group);
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
-    before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
