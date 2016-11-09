f403f6ae343049758a0c611898ce1ced002cb278
asyakasimova <kilyanet@gmail.com>
2016-11-09 12:00:19 +0300

    проблемы с методом isIssueOpen в TestBase

--- rest-sample/src/test/java/ru/stq/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stq/pft/rest/RestAssuredTests.java
@@ -4,10 +4,12 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -16,10 +18,15 @@ import java.util.Set;
 /**
  * Created by A.Kasimova on 06.11.2016.
  */
-public class RestTests extends TestBase{
+public class RestAssuredTests {
+
+  @BeforeClass
+  public void init() {
+    RestAssured.authentication = RestAssured.basic("LSGjeU4yP1X493ud1hNniA==", "");
+  }
+
   @Test
   public void testCreateIssue() throws IOException {
-    skipIfNotFixed(9);
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test Issue").withDescription("New Test Issue");
     int issueId = createIssue(newIssue);
@@ -29,22 +36,17 @@ public class RestTests extends TestBase{
   }
 
   private int createIssue(Issue newIssue) throws IOException {
-    String json = getExecutor().execute(Request.Post("http://demo.bugify.com/api/issues.json")
-            .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
-                    new BasicNameValuePair("description", newIssue.getDescription())))
-            .returnContent().asString();
+    String json = RestAssured.given()
+            .parameter("subject", newIssue.getSubject())
+            .parameter("description", newIssue.getDescription()).post("http://demo.bugify.com/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     return parsed.getAsJsonObject().get("issue_id").getAsInt();
   }
 
   private Set<Issue> getIssues() throws IOException {
-    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues.json")).returnContent().asString();
+    String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-  }
-
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
   }
 }
---
+++ rest-sample/src/test/java/ru/stq/pft/rest/TestBase.java
@@ -0,0 +1,57 @@
+package ru.stq.pft.rest;
+
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+
+import org.testng.SkipException;
+
+import java.io.IOException;
+
+/**
+ * Created by A.Kasimova on 24.09.2016.
+ */
+public class TestBase {
+
+
+//  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+/*  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.bak", "config_inc.php");
+    app.stop();
+  }
+  */
+
+  public boolean isIssueOpen(int issueId) throws IOException {
+
+    String json = getExecutor().execute(Request.Get("http://demo.bugify.com/api/issues/" + issueId + ".json")).returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issue = parsed.getAsJsonArray().get(0);
+    JsonElement state = issue.getAsJsonObject().get("state");
+
+    if (!state.equals("2") && !state.equals("3")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "");
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+}
